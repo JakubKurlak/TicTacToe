c99c218798fc473b5305a07ae1809ec873776786
Jakub Kurlak <j.a.kurlak@gmail.com>
2020-09-05 23:51:01 +0200

    Add method

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,30 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        int size = 6;
-        char[][] table = new char[size][size];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int size = 3;
+        char[][] board1 = new char[size][size];
 
-        char[][] exampleTable = {{'O', 'X', 'X'},
-                                 {'O', 'X', ' '},
-                                 {' ', 'O', 'O'}};
+        char[][] board = {{'O', 'X', 'X'},
+                          {'O', 'X', ' '},
+                          {' ', 'O', 'O'}};
 
-
-        for (int i = 0; i < size ; i++) {
-            System.out.print("\t" + i + ":");
-        }
-        System.out.println();
-
-        for (int row = 0; row < table.length; row++) {
-            System.out.print(row + ": " +"\t");
-            for (int column = 0; column < table[row].length; column++) {
-                System.out.print(table[row][column] + "\t");
-            }
-            System.out.println();
-        }
-
-
-
+        TicTacToe.printBoard(board, size);
 
 
     }
 
+    public static void printBoard(char[][] board, int size) {
+        //column headers
+        for (int i = 0; i < size; i++) {
+            System.out.print("\t" + i + ":");
+        }
+        System.out.println();
+
+        //printing rows
+        for (int row = 0; row < size; row++) {
+            System.out.print(row + ": " + "\t");
+            for (int column = 0; column < size; column++) {
+                System.out.print(board[row][column] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
