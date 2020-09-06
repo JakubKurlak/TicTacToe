675fbcd647a99eb79ba79c17eaebb1bb1ec7954a
Jakub Kurlak <j.a.kurlak@gmail.com>
2020-09-06 02:37:56 +0200

    Add method to win

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,30 +8,87 @@ public class TicTacToe {
     public static volatile int size;
     public static int counter = 0;
 
+
     public static void main(String[] args) throws IOException {
         System.out.println("Hello there, enter the size of the board");
         size = TicTacToe.enterTheSizeOfTheBoard();
         char[][] board = new char[size][size];
         System.out.println("Choose your symbol X or O");
         TicTacToe.ChooseYourSymbol();
-        TicTacToe.printBoard(board, size);
-        while (counter < size * size) {
-            System.out.println("Your symbol is: " + actualSymbol);
-            System.out.println("Enter the row index");
-            int row = TicTacToe.enterTheRow();
-            System.out.println("Enter the column index");
-            int column = TicTacToe.enterTheColumn();
-            boolean correctMove = board[row][column] == 0;
-            if(!correctMove){
-                System.out.println("Invalid move!");
-                continue;
-            }
-            board[row][column] = actualSymbol;
-            TicTacToe.printBoard(board, size);
-            actualSymbol = actualSymbol == 'X' ? 'O' : 'X';
-            counter++;
-        }
 
+        boolean Continue = true;
+        while (counter < size * size && Continue) {
+            TicTacToe.printBoard(board, size);
+            boolean correctMove = TicTacToe.makeMove(board);
+            if (correctMove) {
+                boolean isWinnerInRow = checkIsWinnerInRow(board, actualSymbol);
+                boolean isWinnerInColumn = checkIsWinnerInColumn(board, actualSymbol);
+                boolean isWinnerInCross1 = checkIsWinnerInCross1(board, actualSymbol);
+                boolean isWinnerInCross2 = checkIsWinnerInCross2(board, actualSymbol);
+                if (isWinnerInRow || isWinnerInColumn || isWinnerInCross1 || isWinnerInCross2) {
+                    System.out.println(actualSymbol + " win. \n Congratulations !!!");
+                    Continue = false;
+                }
+                actualSymbol = actualSymbol == 'X' ? 'O' : 'X';
+                counter++;
+            }
+        }
+    }
+
+    public static boolean checkIsWinnerInRow(char[][] board, char actualSymbol) {
+        //win if are three symbols in row
+        for (int row = 0; row < size; row++) {
+            boolean win = true;
+            for (int column = 0; column < size; column++) {
+                if (board[row][column] != actualSymbol) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkIsWinnerInColumn(char[][] board, char actualSymbol) {
+        //win if are three symbols in column
+        for (
+                int column = 0;
+                column < size; column++) {
+            boolean win = true;
+            for (int row = 0; row < size; row++) {
+                if (board[row][column] != actualSymbol) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkIsWinnerInCross1(char[][] board, char actualSymbol) {
+        //win if are three symbols in cross
+        for (int i = 0; i < size; i++) {
+            if (board[i][i] != actualSymbol) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkIsWinnerInCross2(char[][] board, char actualSymbol) {
+        //win if are three symbols in cross
+        if (board[0][2] != actualSymbol ||
+                board[1][1] != actualSymbol ||
+                board[2][0] != actualSymbol) {
+            return false;
+        }
+        return true;
     }
 
     public static void printBoard(char[][] board, int size) {
@@ -119,5 +176,20 @@ public class TicTacToe {
             }
         } while (true);
         return column;
+    }
+
+    public static boolean makeMove(char[][] board) throws IOException {
+        System.out.println("Your symbol is: " + actualSymbol);
+        System.out.println("Enter the row index");
+        int row = TicTacToe.enterTheRow();
+        System.out.println("Enter the column index");
+        int column = TicTacToe.enterTheColumn();
+        boolean correctMove = board[row][column] == 0;
+        if (!correctMove) {
+            System.out.println("Invalid move!");
+            return false;
+        }
+        board[row][column] = actualSymbol;
+        return true;
     }
 }
